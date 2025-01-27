ee540d29594f7032c86a6fdace431f9755b2184c
jonathan skarman <kaboomgf@gmail.com>
2025-01-27 09:26:37 +0100

    rewrote response
    generalized session response method, and made extra filetypes need to go through status method

--- lib/response.rb
+++ lib/response.rb
@@ -5,55 +5,43 @@ class Response
 	def initialize; end # rubocop:disable Style/RedundantInitialize
 
 	def send(route, session)
-		if route == '404'
-			html = '<h1>404 Not Found</h1>'
-			status = '404'
-			session_response_html(status, html, session)
-		elsif route.include?('.css')
-			status = '200'
-			css = File.read(route)
-			session_response_css(status, css, session)
-		else
-			if route.include?('.slim')
-				html, status = slim_status(route)
-			elsif route.include?('.html')
-				html, status = html_status(route)
-			else
-				raise 'invalid route - neither html nor slim'
-			end
-			session_response_html(status, html, session)
-		end
+		content, status, content_type = status(route)
+		session_response(status, content, content_type, session)
 	end
 
 	private
 
-	def html_status(route)
-		html = File.read(route)
-		status = '200'
-		[html, status]
+	# ja den är alldeles för lång, men liksom, det är en bara en if else. får skriva om någon annan gång
+	def status(route) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+		if route == '404'
+			content = '<h1>404 Not Found</h1>'
+			status = '404'
+			content_type = 'text/html'
+		elsif	route.include?('.css')
+			content = File.read(route)
+			status = '200'
+			content_type = 'text/css'
+		elsif route.include?('.slim')
+			layout_template = Slim::Template.new('views/layout.slim')
+			content_template = Slim::Template.new(route)
+			content = layout_template.render { content_template.render } # makes layout and routed slim files work
+			status = '200'
+			content_type = 'text/html'
+		elsif route.include?('.html')
+			content = File.read(route)
+			status = '200'
+			content_type = 'text/html'
+		else
+			raise 'invalid route in status method, in response class'
+		end
+		[content, status, content_type]
 	end
 
-	def slim_status(route)
-		layout_template = Slim::Template.new('views/layout.slim')
-		content_template = Slim::Template.new(route)
-		html = layout_template.render { content_template.render } # makes layout and routed slim files work
-		status = '200'
-		[html, status]
-	end
-
-	def session_response_html(status, html, session)
+	def session_response(status, content, content_type, session)
 		session.print "HTTP/1.1 #{status}\r\n"
-		session.print "Content-Type: text/html\r\n"
+		session.print "Content-Type: #{content_type}\r\n" # content_type = "text/css" / "text/html"
 		session.print "\r\n"
-		session.print html
-		session.close
-	end
-
-	def session_response_css(status, css, session)
-		session.print "HTTP/1.1 #{status}\r\n"
-		session.print "Content-Type: text/css\r\n"
-		session.print "\r\n"
-		session.print css
+		session.print content
 		session.close
 	end
 end
